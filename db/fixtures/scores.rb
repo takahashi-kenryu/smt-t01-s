217d48ff4f993bb56f5eb4ae020b648dac573580
takahashi <>
2018-09-18 10:56:51 +0900

    delete scores

--- db/fixtures/scores.rb
+++
@@ -1,5 +0,0 @@
-Score.seed do |s|
-  s.id = 1
-  s.name = 'test'
-  s.value = 1000
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_072829) do
-
-  create_table "scores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "name"
-    t.integer "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
